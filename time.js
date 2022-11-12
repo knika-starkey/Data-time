d8cdc1e323e71a69461a2555f1fa647db46e505d
knika-starkey <knika787@gmail.com>
2022-11-13 01:27:38 +0200

    timer and time

--- time.js
+++ time.js
@@ -17,6 +17,30 @@
 //     ${h}; минут -
 //     ${m}; секунд -
 //     ${s}`);
+// Установить дату и время в будущем
+let date_in_future = new Date(2023, 0, 1, 0, 0, 0); // 1.01.2020, 10:32:04
+let x;
+function timer() {
+  // Установить дату и время текущего момента
+  let date = new Date().getTime();
+  // Разница в милисекундах между текущей и будущей датой
+  let ms = date_in_future - date;
+  // Подсчет разницы времени в днях, часах, минутах и секундах
+  let days = Math.floor(ms / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((ms % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((ms % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((ms % (1000 * 60)) / 1000);
+  // Показать результат в теге с id="time"
+  document.getElementById(
+    "timer"
+  ).innerHTML = `До нового року залишилось: ${days} днів ${hours} годин ${minutes} хвилин ${seconds} секунд`;
+  // Если время истекло, показать сообщение
+  if (ms < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "Час закінчився!";
+  }
+  x = setInterval(timer, 1000);
+}
 
 function startTime() {
   let date = new Date();
@@ -26,7 +50,8 @@ function startTime() {
   if (hours < 10) hours = "0" + hours;
   if (minutes < 10) minutes = "0" + minutes;
   if (seconds < 10) seconds = "0" + seconds;
-  document.getElementById("time").innerHTML =
-    hours + ":" + minutes + ":" + seconds; // Выбор элемента документа по его id = "time"
+  document.getElementById(
+    "timeNow"
+  ).innerHTML = `Зараз: ${hours}:${minutes}:${seconds}`; // Выбор элемента документа по его id = "time"
   setInterval(startTime, 1000);
 }
